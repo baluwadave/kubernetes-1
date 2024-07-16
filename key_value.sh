6909a2b08a989e9da6b7a76418f684204056f84a
balu <baluwadave@gmail.com>
2024-07-16 21:17:51 +0530

    --- Auto Git Commit ---

--- key_value.sh
+++ key_value.sh
@@ -9,21 +9,3 @@ My_Array=(
 echo "my name is ${My_Array[name]} "
 echo "and my age is ${My_Array[age]} "
 echo "and living in ${My_Array[city]} city"
-
-
-#!/bin/bash
-
-# Declare the associative array
-declare -A My_Array
-
-# Initialize the array elements
-My_Array=(
-    [name]="Balu"
-    [age]=28
-    [city]="Pune"
-)
-
-# Access and print the elements of the array
-echo "My name is ${My_Array[name]}"
-echo "And my age is ${My_Array[age]}"
-echo "And living in ${My_Array[city]} city"
