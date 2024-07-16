21a1218f951a23c057ecf9e8de2eca71c9538e5f
balu <baluwadave@gmail.com>
2024-07-16 21:27:55 +0530

    --- Auto Git Commit ---

---
+++ string.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+My_Var = "Hey Buddy How are you?"
+
+
+My_Var_Length=${#My_Var}
+echo "Length of the My_Var is $My_Var_Length"
+
+echo "Upper case is -------${My_Var^^}"
+
+echo "Lower case is -------${My_Var,,}"
+
+#To replace the string 
+New_Var= ${My_Var/Buddy/Balu}
+echo "New Var is ---- $New_Var"
