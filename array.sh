c65b5c38b3f4796c77177e3ae077092a6f2997ea
balu <baluwadave@gmail.com>
2024-07-16 16:23:19 +0530

    --- Auto Git Commit ---

--- array.sh
+++ array.sh
@@ -3,3 +3,7 @@ My_Array=(1 20 30.5 Hello "Hello buddys")
 echo "print the full array ${My_Array[*]}"
 
 echo "print the 3 element of array ${My_Array[3]}"
+
+
+
+echo "length of an array ${#My_Array[*]}"
