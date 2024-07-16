142a3c81ec40578344e9d0bb79de8f4335180006
balu <baluwadave@gmail.com>
2024-07-16 20:59:15 +0530

    --- Auto Git Commit ---

--- array.sh
+++ array.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 My_Array=(1 20 30.5 Hello "Hello buddys")
+#printing all array element
 echo "print the full array ${My_Array[*]}"
+
+#accessing array element 
 
 echo "print the 3 element of array ${My_Array[3]}"
 
-
-
+#finding the length of array
 echo "length of an array ${#My_Array[*]}"
+
+#updating array element
+My_Array+=(5 6 10)
+echo "print updated array: ${My_Array[*]}"
