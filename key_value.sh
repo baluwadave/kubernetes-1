eac85ced2f02c05bcc8c8d63f38eaa1c9a2fd2f6
balu <baluwadave@gmail.com>
2024-07-16 21:07:18 +0530

    --- Auto Git Commit ---

--- key_value.sh
+++ key_value.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 declare -A My_Array 
-My_Array = ([name]=Balu [age]=28 [city]=Pune)
+My_Array =( [name]=Balu [age]=28 [city]=Pune )
 
-echo "my name is ${My_Array[name]} and my age is ${My_Array[age]} and living in ${My_Array[city]}"
+echo "my name is ${My_Array[name]} "
+echo "and my age is ${My_Array[age]} "
+echo "and living in ${My_Array[city]} city"
