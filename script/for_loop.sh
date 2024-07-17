1f303e0ca6a0a06a0190802588f9032c6a336e34
balu <baluwadave@gmail.com>
2024-07-17 12:34:00 +0530

    --- Auto Git Commit ---

--- script/for_loop.sh
+++ script/for_loop.sh
@@ -2,3 +2,9 @@ for  i in {1..20}
 do 
         echo "Number is $i"
 done        
+
+FILE="/d/kubernetes-1/kubernetes-1/script/name.txt"
+for name in $(cat $FILE)
+do      
+        echo "Name is $name"
+done        
