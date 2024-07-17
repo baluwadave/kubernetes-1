81be57242b5a441196b0b2c285cdf3cc867b75fd
balu <baluwadave@gmail.com>
2024-07-17 13:57:38 +0530

    --- Auto Git Commit ---

--- script/function.sh
+++ script/function.sh
@@ -2,9 +2,11 @@
 num1=$(read -p "enter num1 value")
 num1=$(read -p "enter num2 value")
 
-addition(){
+addition( $num1 $num2){
         # local num1=$1
         # local num2=$2
         let sum=$num1+$num2
     echo "Sum of $num1 and $num2 is $sum"    
 }
+
+addition
