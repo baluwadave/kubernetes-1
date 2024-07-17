678fc27b11804b42c0e41d72a17509b6f405e11d
balu <baluwadave@gmail.com>
2024-07-17 14:39:58 +0530

    --- Auto Git Commit ---

---
+++ script/break.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+num=8
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+    if [[ $num -eq $i]]
+    then
+            echo "$num is Found !!!"
+            break
+    fi        
+    echo "Number is $i"
+done    
