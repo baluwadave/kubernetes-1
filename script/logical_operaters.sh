66f7d1fa0fd08a634405629995beb8856f1cf5ee
balu <baluwadave@gmail.com>
2024-07-17 12:19:15 +0530

    --- Auto Git Commit ---

--- script/logical_operaters.sh
+++ script/logical_operaters.sh
@@ -4,7 +4,7 @@ read -p "what is your age:" age
 read  -p "your country : " country
 
 
-if [[ $age -ge 18 ]] && [[ $countr -eq "India" ]]
+if [[ $age -ge 18 ]] && [[ $country == "India" ]]
 then
         echo "you can vote "
 else       
